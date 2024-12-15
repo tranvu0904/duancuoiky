8717820218d86c54bdf186b41d8bbe6e7b6abf50
tranvu0904 <quangvu2820@gmail.com>
2024-12-15 22:44:08 +0700

    Fix Home.js for search bar visibility

--- client/src/containers/Public/Home.js
+++ client/src/containers/Public/Home.js
@@ -3,48 +3,60 @@ import Header from "./Header";
 import { Outlet, useLocation } from "react-router-dom";
 import { Navigation, Search } from "./index";
 import { Contact } from "../../components";
-import { useSelector } from "react-redux";
 import { path } from "../../utils/constant";
 
 function Home() {
-  const { isLoggedIn } = useSelector((state) => state.auth);
   const navRef = useRef();
   const location = useLocation();
+
+  // Xử lý sự kiện scroll để cố định Navigation
   useEffect(() => {
-    const handleScroll = (e) => {
+    const handleScroll = () => {
       if (window.pageYOffset >= 134) {
         navRef.current.style.cssText = `
         position: fixed;
         top: 0;
         left: 0;
         right: 0;
-        z-index:50;
+        z-index: 50;
         `;
       } else {
-        navRef.current.style.cssText = `
-        width: 100%
-        `;
+        navRef.current.style.cssText = `width: 100%;`;
       }
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  // Các trang cần hiển thị thanh tìm kiếm
+  const allowedPaths = [
+    "/",
+    `/${path.HOME__PAGE}`,
+    `/${path.CHO_THUE_CAN_HO}`,
+    `/${path.CHO_THUE_MAT_BANG}`,
+    `/${path.CHO_THUE_PHONG_TRO}`,
+    `/${path.NHA_CHO_THUE}`,
+    `/${path.SEARCH}`,
+  ];
+
+  const isShowSearch = allowedPaths.includes(location.pathname);
+
   return (
     <div className="w-full flex flex-col items-center m-auto h-full">
       <Header />
       <div ref={navRef} className="w-full">
         <Navigation />
       </div>
-      {/* {isLoggedIn &&
-        location.pathname !== `/${path.CONTACT}` &&
-        !location.pathname.includes(path.DETAIL) && <Search />} */}
-      <Search />
+
+      {/* Chỉ hiển thị Search trên các trang được phép */}
+      {isShowSearch && <Search />}
+
       <div className="w-3/4 flex flex-col items-center justify-center">
         <Outlet />
       </div>
-      {/* <Intro /> */}
       <Contact />
       <div className="h-6"></div>
     </div>
